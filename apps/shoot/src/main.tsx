9a4d46244a7f15c9054a2669c2f7323a2c98683b
bryan.y <bryan.y@binance.com>
2024-11-05 17:11:01 +0800

    feat: tailwind setup

--- apps/shoot/src/main.tsx
+++ apps/shoot/src/main.tsx
@@ -2,9 +2,10 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import './polyfill';
-import './env'
+import './env';
 
 import { router } from './router';
+import './tailwind.css';
 
 const rootElement = document.getElementById('root')!;
 createRoot(rootElement).render(<RouterProvider router={router} />);
